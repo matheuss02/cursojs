e0140a8c86a8fd66355db675ebe896ce168c7a04
matheuss02 <matheus.moreira02@escola.pr.gov.br>
2025-09-18 12:39:51 -0300

    Add files via upload

---
+++ cursojs/cursojs/cursojs/exer10.js
@@ -0,0 +1,18 @@
+let a  =  10;
+let b = 5;
+
+// operadores aritméticos
+console.log("soma:", a + b);
+console.log("subtracao:", a - b);
+console.log("multiplicacao:", a * b);
+console.log("divisao:", a / b);
+console.log("resto;", a % b);
+
+//operadores lógicos e relacionais
+console.log("a é maior que b?", a > b);
+console.log("a é maior que b?", a < b);
+console.log("a é igual a b?", a === b);
+
+// Operador lógico AND e OR
+let condicao = (a > 0 && b > 0) || (a < 0);
+console.log("condição lógica:", condicao);
